92be6eaca231fc0dd1a67358d9891aa9cb225325
npmitchell <npmitchell@kitp.ucsb.edu>
2020-12-28 10:26:52 -0800

    Updates to NES_codes/ for cylinder simulation pinching

--- NES_codes/aux_plot_beta_pattern.m
+++ NES_codes/aux_plot_beta_pattern.m
@@ -1,14 +1,14 @@
-function aux_plot_beta_pattern(betaCut, Vc, eIDxCut, outfn)
+function aux_plot_beta_pattern(beta, V, eIDx, outfn)
 
 cmin_beta = 0; cmax_beta = 1 ;
-cmap = parula ;
-colormap parula
-cID = max(1, sum(abs(sin(betaCut)) > linspace(cmin_beta, cmax_beta, length(cmap)), 2)) ;
+cmap = jet ;
+cID = max(1, sum(abs(sin(beta)) > linspace(cmin_beta, cmax_beta, length(cmap)), 2)) ;
 ecolors = cmap(cID, :) ;
 figure('visible', 'off')
-plotColoredLinesegs([Vc(eIDxCut(:,1), :), Vc(eIDxCut(:, 2), :)], ecolors, ...
-    'linewidth', 10^3 / length(Vc)) ;
+plotColoredLinesegs([V(eIDx(:,1), :), V(eIDx(:, 2), :)], ecolors, ...
+    'linewidth', 10^3 / length(V)) ;
 c = colorbar ;
+colormap jet
 caxis([cmin_beta, cmax_beta])
 c.Color = 'w' ;
 c.Label.Interpreter = 'latex' ;
@@ -19,5 +19,6 @@ set(gcf, 'color', 'k')
 title('axial angle $\beta$ definition', 'interpreter', 'latex', 'color', 'w'); 
 axis equal
 axis off
+drawnow
 % save figure
 export_fig(outfn, '-r300', '-nocrop') ;
--- NES_codes/aux_plot_strain_pattern.m
+++ NES_codes/aux_plot_strain_pattern.m
@@ -1,27 +1,28 @@
 function aux_plot_strain_pattern(V, eIDx, eL0, distribution,...
-    strainstyle, strainStruct, cmin, cmax, outfn)
+    strainstyle, strainStruct, cmin, cmax, outfn, Rad)
 
 
-midpts = 0.5 * (V(eIDx(:, 1), 1) + V(eIDx(:, 2), 1)) ;
-% midy = 0.5 * (V(eIDx(:, 1), 2) + V(eIDx(:, 2), 2)) ;
+midx = 0.5 * (V(eIDx(:, 1), 1) + V(eIDx(:, 2), 1)) ;
+midy = 0.5 * (V(eIDx(:, 1), 2) + V(eIDx(:, 2), 2)) ;
+midz = 0.5 * (V(eIDx(:, 1), 3) + V(eIDx(:, 2), 3)) ;
+midpt = [midx(:), midy(:), midz(:)] ;
+
 beta = strainStruct.beta ;
 
 switch distribution
     case 'gaussian'
-        mag = exp(- midpts.^2/ (2 * strainStruct.sigma^2)) ;
+        mag = exp(- midx.^2/ (2 * strainStruct.sigma^2)) ;
 end
 
 switch strainstyle
     case 'isopulse'
         scalefactor = 1 - mag ;  
     case 'hoopstrain'
-        disp('strain style is hoopstrain')
         scalefactor = 1 - mag .* abs(sin(beta)) ;
-        % scatter(midpts + rand(size(midpts)), scalefactor, 50, beta, 'filled')
-        % colorbar
-        % waitfor(gcf)
     case 'halfhoop'
-        scalefactor = 1 - mag .* abs(sin(beta)) ;
+        % target strain is zero at midplane and above, varies to mag at z=-Rad
+        scalefactor = 1 + mag .* abs(sin(beta)) .* (midy/Rad);
+        scalefactor(scalefactor > 1) = 1 ;
 end
 eL = eL0 .* scalefactor ;
 estrain = (eL - eL0) ./ eL0 ;
@@ -32,18 +33,24 @@ ecolors = cmap(cID, :) ;
 figure('visible', 'off')
 plotColoredLinesegs([V(eIDx(:,1), :), V(eIDx(:, 2), :)], ecolors, ...
     'linewidth', 10^3 / length(V)) ;
+drawnow
 c = colorbar ;
+colormap bwr
 caxis([cmin, cmax])
 c.Color = 'w' ;
 c.Label.Interpreter = 'latex' ;
-c.Label.String = '$\epsilon$' ;
+c.Label.String = '$\varepsilon$' ;
 % Figure properties
 set(gca, 'color', 'k', 'xcol', 'w', 'ycol', 'w')
 set(gcf, 'color', 'k')
 title('bond strain pattern', 'interpreter', 'latex', 'color', 'w'); 
 axis equal
-view(2);
 axis off
+drawnow
 % save figure
 export_fig(outfn, '-r300', '-nocrop') ;
+
+
+
+
 %%%%%%%
